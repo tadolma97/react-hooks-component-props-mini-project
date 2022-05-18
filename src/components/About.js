44a0152b5ecb2aefe9ed3482714c7e6ed380512b
Tashi Dolma <tadolma97@gmail.com>
2022-05-17 23:02:04 -0400

    commit tree 82212aa473df7a977c6ee0eb017ddf0b1525b2f1

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+function About({image="https://via.placeholder.com/215 (Links to an external site.)", about}){
+    return (
+        <aside>
+            <img src={image} alt='blog logo'></img>
+            <p>{about}</p>
+        </aside>
+    )
+}
+
+export default About
